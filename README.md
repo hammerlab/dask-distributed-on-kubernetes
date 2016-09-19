de1bfcdea0ea4f599a9aaeea0654c853cd8fc198
Tim O'Donnell <timodonnell@gmail.com>
2016-09-19 16:59:50 -0400

    better readme

--- README.md
+++ README.md
@@ -1,34 +1,30 @@
 # Running on kubernetes on google container engine
 
+This small repo gives an example Kubernetes configuration for running [dask.distributed](https://github.com/dask/distributed) on Google Container Engine.
+
 ## Start a cluster if needed
 
-I called mine "tim-ml1":
+If you don't already have a cluster running, use a command like the following to start one (here it is called "daskd-cluster"):
 
 ```
-gcloud container clusters create tim-ml1 \
+gcloud container clusters create daskd-cluster \
     --zone us-east1-b --num-nodes=2 \
     --enable-autoscaling --min-nodes=1 --max-nodes=100 \
     --machine-type=n1-standard-32
 ```
 
-It should show up here:
+You should see your cluster:
 https://console.cloud.google.com/kubernetes/list
 
-Run this:
+Then run this to set it as the default for your session:
 ```
-gcloud config set container/cluster tim-ml1
-gcloud container clusters get-credentials tim-ml1
+gcloud config set container/cluster daskd-cluster
+gcloud container clusters get-credentials daskd-cluster
 ```
 
 ## Deploy dask distributed
 
-If you want to use a development checkout of MHCflurry, first build a new MHCflurry docker image. From the MHCflurry checkout, run:
-
-```
-docker build .
-```
-
-When that completes, tag it, push it to docker hub, and edit `spec.yaml` to point to your image.
+You will want to edit [spec.yaml](spec.yaml) to use the docker image appropriate for your task. You may also want to customize the CPU and memory thresholds requested based on what's required for your task.
 
 This will launch dask scheduler and one worker:
 
@@ -36,13 +32,13 @@ This will launch dask scheduler and one worker:
 kubectl create -f spec.yaml
 ```
 
-Can check it like this:
+You can check how many workers are running with:
 
 ```
 kubectl get pods
 ```
 
-Get the IP of the scheduler (you want the external ip of daskd-scheduler):
+Now, get the IP of the scheduler (you want the external ip of daskd-scheduler):
 
 ```
 $ kubectl get service
@@ -51,39 +47,58 @@ daskd-scheduler   10.3.249.60   104.196.185.187   8786/TCP   4m
 kubernetes        10.3.240.1    <none>            443/TCP    17h
 ```
 
-Then scale it up:
-
+Sometimes this one-liner is handy for getting the IP:
 ```
-kubectl scale deployment daskd-worker --replicas=400
-```
-
-## Run analysis
-
-Run mhcflurry-class1-allele-specific-cv-and-train, passing in the host and IP of the scheduler above, e.g. `--dask-scheduler 104.196.185.187:8787`.
-
-## When finished (important)
-
-Run:
-```
-kubectl delete -f spec.yaml
-gcloud container clusters delete tim-ml1
+DASK_IP=$(kubectl get service | grep daskd-scheduler | tr -s ' ' | cut -d ' ' -f 3)
 ```
 
-
-## Other commands
-
-To resize the cluster (probably not necessary if auto scaling):
+Now, scale up the deployment. Here we request 100 workers:
 
 ```
-gcloud container clusters resize CLUSTER_NAME --size SIZE
+kubectl scale deployment daskd-worker --replicas=100
 ```
 
-Look at logs (exact name will be different):
+You can now run `kubectl get pods` again to check when the workers are started.
+
+You can check on a worker's stdin/stdout with (replace the name with a pod name from `kubectl get pods`):
 
 ```
 kubectl logs daskd-scheduler-3680716393-j19xr
 ```
 
+## Run your analysis
 
+When you instantiate your dask Executor, just pass in the IP and port:
 
+```python
+from math import sqrt
+from dask.distributed import Executor
+from dask import delayed
 
+client = Executor("104.196.185.187:8786")
+tasks = [dask.delayed(sqrt)(i) for i in range(100)]
+results = client.compute(tasks, sync=True)
+print(results)
+```
+
+## Tearing it down
+
+When you're done, shut down the service and cluster:
+```
+kubectl delete -f spec.yaml
+gcloud container clusters delete daskd-cluster
+
+## Running a benchmark
+
+We also include a simple [benchmark](benchmarking/benchmark.py) script that will test performance of the cluster with varying numbers of workers (it issues `kubectl` calls itself to change the number of workers). See the script for details. Here's an example invocation:
+
+```
+DASK_IP=$(kubectl get service | grep daskd-scheduler | tr -s ' ' | cut -d ' ' -f 3)
+python benchmark.py \
+    --tasks 5000 \
+    --task-time .05 \
+    --dask-scheduler $DASK_IP:8786 \
+    --jobs-range 200 800 200 \
+    --replicas 1 \
+    --out results2.csv
+```
--- benchmarking/benchmark.py
+++ benchmarking/benchmark.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Dask distributed joblib backend on kubernetes benchmark script
+Dask distributed on kubernetes benchmark script
 """
 
 import argparse
@@ -10,10 +10,10 @@ import time
 import subprocess
 import socket
 
-import joblib
 import numpy
 
-import distributed.joblib  # for side effects
+import dask
+from distributed import Executor
 
 parser = argparse.ArgumentParser(description=__doc__)
 
@@ -71,17 +71,6 @@ parser.add_argument(
     help="")
 
 parser.add_argument(
-    "--joblib-num-jobs",
-    type=int,
-    default=1,
-    help="Set to -1 to use as many jobs as cores")
-
-parser.add_argument(
-    "--joblib-pre-dispatch",
-    default='2*n_jobs',
-    help="")
-
-parser.add_argument(
     "--quiet",
     action="store_true",
     default=False,
@@ -98,7 +87,6 @@ parser.add_argument(
     help="")
 
 
-
 def make_data(size_mb):
     if not size_mb:
         return None
@@ -111,10 +99,10 @@ def task(task_data, task_time, task_allocate_mb, task_output_mb):
     return (socket.gethostname(), make_data(task_output_mb))
 
 
-def go(args, cores, out_fds):
+def go(client, args, cores, out_fds):
     for replica in range(args.replicas):
         tasks = [
-            joblib.delayed(task)(
+            dask.delayed(task)(
                 make_data(args.task_input_mb),
                 args.task_time_sec,
                 args.task_allocate_mb,
@@ -122,15 +110,12 @@ def go(args, cores, out_fds):
             for _ in range(args.tasks)
         ]
         start = time.time()
-        results = joblib.Parallel(
-            n_jobs=args.joblib_num_jobs,
-            verbose=1 if not args.quiet else 0,
-            pre_dispatch=args.joblib_pre_dispatch)(tasks)
+        results = client.compute(tasks, sync=True)
+        print(results)
         length = time.time() - start
 
         assert len(results) == args.tasks
 
-        #logging.info("Hosts: %s" % " ".join(set([x[0] for x in results])))
         logging.info("Hosts: %s" % len(set([x[0] for x in results])))
 
         for fd in out_fds:
@@ -148,6 +133,7 @@ def go(args, cores, out_fds):
             fd.write("\n")
             fd.flush()
 
+
 if __name__ == "__main__":
     args = parser.parse_args(sys.argv[1:])
     if not args.quiet:
@@ -157,36 +143,32 @@ if __name__ == "__main__":
     out_fds = [sys.stdout]
     if args.out:
         out_fds.append(open(args.out, 'w'))
+
+    client = None
     if args.dask_scheduler:
-        backend = joblib.parallel_backend(
-            'distributed',
-            scheduler_host=args.dask_scheduler)
-        with backend:
-            active_backend = joblib.parallel.get_active_backend()[0]
-            logging.info(
-                "Running with dask scheduler: %s [%d cores]" % (
-                    args.dask_scheduler,
-                    active_backend.effective_n_jobs()))
-
-            if args.jobs_range is not None:
-                for i in range(*args.jobs_range):
-                    command = args.scale_command % i
-                    logging.info("Running: %s" % command)
-                    subprocess.check_call(command, shell=True)
-                    while True:
-                        cores = active_backend.effective_n_jobs(n_jobs=args.joblib_num_jobs)
-                        logging.info("Cores: %d. Waiting for %d cores." % (cores, i))
-                        if cores == i:
-                            break
-                        time.sleep(1)
-                    go(args, cores, out_fds)
-            else:
-                cores = active_backend.effective_n_jobs(n_jobs=args.joblib_num_jobs)
-                go(args, cores, out_fds)
-
+        client = Executor(args.dask_scheduler)
     else:
-        active_backend = joblib.parallel.get_active_backend()[0]
-        cores = active_backend.effective_n_jobs(n_jobs=args.joblib_num_jobs)
-        logging.info(
-                "Running with joblib scheduler [%d cores]" % cores)
-        go(args, cores, out_fds)
+        client = Executor()
+
+    print(dir(client))
+    logging.info(
+        "Running with dask scheduler: %s [%s cores]" % (
+            args.dask_scheduler,
+            sum(client.ncores().values())))
+
+    if args.jobs_range is not None:
+        for i in range(*args.jobs_range):
+            command = args.scale_command % i
+            logging.info("Running: %s" % command)
+            subprocess.check_call(command, shell=True)
+            while True:
+                cores = sum(client.ncores().values())
+                logging.info(
+                    "Cores: %d. Waiting for %d cores." % (cores, i))
+                if cores == i:
+                    break
+                time.sleep(1)
+            go(client, args, cores, out_fds)
+    else:
+        cores = sum(client.ncores().values())
+        go(client, args, cores, out_fds)
